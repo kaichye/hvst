8bd7209924cc03ed0020e3acfc5a8ec6ffca361f
kaichye <74321555+kaichye@users.noreply.github.com>
2022-10-14 15:45:21 -0400

    Update packageSort.sh

--- packageSort.sh
+++ packageSort.sh
@@ -5,7 +5,7 @@ info=$(tput setaf 2)
 reset=$(tput sgr0)
 
 # basically check against a list of default installed packages on debian 10
-apt list --installed | grep -vf ./dataFiles/defaultInstalled.data > ./dataFiles/currentInstalled.data
+apt list --installed | grep -vf ./dataFiles/defaultInstalled.data > ./dataFiles/diffInstalled.data
 
 printf "${info}Check in ./dataFiles/currentInstalled.data${reset}\n\n"
 
