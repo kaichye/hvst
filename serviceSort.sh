46a21a98dda1fb9618f898d70e1d0c8c1156af50
kaichye <74321555+kaichye@users.noreply.github.com>
2022-10-14 15:40:49 -0400

    Update serviceSort.sh

--- serviceSort.sh
+++ serviceSort.sh
@@ -14,7 +14,7 @@ systemctl --type=service | grep -vf ./dataFiles/defaultServices.data
 printf "\n${info}Default Services that are not there${reset}\n"
 
 systemctl --type=service | cut -d " " -f 1 | grep "\S" > ./dataFiles/currentServices.data
-grep -vf ./dataFiles/currentServices.data ./dataFiles/defaultServices.data
+grep -vf ./dataFiles/currentServices.data ./dataFiles/defaultServices.data > ./dataFiles/diffServices.data
 
 printf "\n${info}Make sure to check often! Services will not appear if it is not started!${reset}"
 
